47ed72f62732c4fc573d026bd9b1b19da2a699a4
unknown <abdalrahmansalah002@gmail.com>
2025-07-26 18:26:21 +0300

    upload project to remote repo

---
+++ first_p.py
@@ -0,0 +1,18 @@
+print("Hello world")    
+print("hello\\\
+world")
+print("\xF5");print("""no /""")
+myString="    @@i love networking     "
+print(myString.strip(" @",))
+mstring="hello from the other world"
+x=mstring.find("g")
+'''
+if x==-1:
+    print("not found")
+else:
+    print(type(x))
+    '''
+mString='its one two one three one two one four'
+print(mString.istitle())
+x,y,z="mohamed","networking",10
+print("My name is : %s My major is : %s exYear is : %d " %(x,y,z))
